5450a0ecfb49795be379a0e46b02c4825d1f50b7
JI-doo <awt0311@gmail.com>
2020-09-10 14:53:01 +0900

    Modify profile

--- Carat/Carat/ViewController/MenuBar.swift
+++ Carat/Carat/ViewController/MenuBar.swift
@@ -8,23 +8,55 @@
 
 import UIKit
 
-class MenuBar: UIView {
-    let collectionView: UICollectionView = {
+class MenuBar: UIView, UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+  
+  
+    
+    lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         let cv = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        cv.backgroundColor = UIColor.white
+        cv.dataSource = self
+        cv.delegate = self
         return cv
     }()
     
+    let cellId = "cellId"
     override init(frame: CGRect) {
         super.init(frame: frame)
         
+        collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: cellId)
+        
         addSubview(collectionView)
-        
-        
         backgroundColor = UIColor.white
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int
+    {
+        return 2
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: cellId, for: indexPath)
+        return cell
+        
+        cell.backgroundColor = UIColor.blue
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: 4, height: 0)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 0
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+      
+      
 }
+
+
--- Carat/Carat/ViewController/ProfileController.swift
+++ Carat/Carat/ViewController/ProfileController.swift
@@ -8,7 +8,13 @@
 
 import UIKit
 
-class ProfileController: UIViewController/*, UIScrollViewDelegate*/{
+protocol CustomMenuBarDelegate: class {
+    func customMenuBar(scrollTo index: Int)
+}
+
+
+class ProfileController: UIViewController{
+ 
     var scrollView: UIScrollView!
     var imageView: UIImageView!
     
@@ -22,33 +28,86 @@ class ProfileController: UIViewController/*, UIScrollViewDelegate*/{
     @IBOutlet weak var followernum: UILabel!
     @IBOutlet weak var followingnum: UILabel!
     @IBOutlet weak var modifyButton: UIButton!
-   // @IBOutlet weak var customTapbar: UIView!
-    @IBOutlet weak var scrollViewContainerViewWidth: NSLayoutConstraint!
-      
-
+    @IBOutlet weak var collectionBar: UICollectionView!
+    
+    required init?(coder aDecoder: NSCoder) {
+           fatalError("init(coder:) has not been implemented")
+       }
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
-        scrollViewContainerViewWidth.constant = UIScreen.main.bounds.size.width * 2
+        
+        self.collectionBar.delegate = self
+        self.collectionBar.dataSource = self
+        
+        let selectedIndexPath = NSIndexPath(item: 0, section: 0)
+        collectionBar.selectItem(at: selectedIndexPath as IndexPath, animated: true, scrollPosition: .centeredHorizontally)
+        
+        modifyButton.layer.cornerRadius = 10
+    }
+    var customTabBarCollectionView: UICollectionView = {
+        let collectionViewLayout = UICollectionViewFlowLayout()
+        collectionViewLayout.scrollDirection = .horizontal
+        let collectionView = UICollectionView(frame: CGRect(x: 0, y: 0, width: 0, height: 0), collectionViewLayout: collectionViewLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = UIColor.white
+        return collectionView
+    }()
+    
+    var indicatorView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = UIColor.black
+        return view
+    }()
+    
+    //MARK:  Properties
+    var indicatorViewLeadingConstraint: NSLayoutConstraint!
+    var indicatorViewWidthConstraint: NSLayoutConstraint!
+    
+    //MARK: Setup Views
+    func setupCollectioView() {
+        customTabBarCollectionView.delegate = self
+        customTabBarCollectionView.dataSource = self
+        customTabBarCollectionView.showsHorizontalScrollIndicator = false
+        //customTabBarCollectionView.register(UINib(nibName: CustomCell.reusableIdentifier, bundle: nil), forCellWithReuseIdentifier: CustomCell.reusableIdentifier)
+        customTabBarCollectionView.isScrollEnabled = false
+        
+        let indexPath = IndexPath(item: 0, section: 0)
+        customTabBarCollectionView.selectItem(at: indexPath, animated: false, scrollPosition: [])
     }
     
-    @IBAction func onAddChildController(_ sender: Any) {
-        let storyBoard = UIStoryboard(name: "Other", bundle: nil)
-        let addViewController = storyboard?.instantiateViewController(withIdentifier: "AddViewController")
-        
-       // self.addChild(addViewController)
-       // self.view.addSubview(addViewController.view)
-        
-        addViewController?.didMove(toParent: self)
-        
+    func setupCostomTabBar() {
+        setupCollectioView()
+        self.view.addSubview(customTabBarCollectionView)
+       // customTabBarCollectionView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
+       // customTabBarCollectionView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
+       // customTabBarCollectionView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
+        customTabBarCollectionView.heightAnchor.constraint(equalToConstant: 55).isActive = true
         
     }
-  
+
 }
-extension ViewController: UIScrollViewDelegate {
-    func scrollViewDidScroll(_ scrollView: UIScrollView) {
-        print("Current X,Y are X:\(scrollView.contentOffset.x), Y: \(scrollView.contentOffset.y)")
-        let currentPage = Int(round(scrollView.contentOffset.x / scrollView.frame.size.width))
-        print("current page : \(currentPage)")
+
+extension ProfileController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 2
     }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: TopTabBarCell = collectionView.dequeueReusableCell(withReuseIdentifier: "topTabBar", for: indexPath) as! TopTabBarCell
+        
+        let arrayText = ["캐링","캐럿"]
+        cell.titleLabel.text = arrayText[indexPath.row]
+        
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+           return 0
+    }
+    
 }
+
